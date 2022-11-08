1111a4e923a7027ff0727eb32dc1fec071dd0ccf
Paweł Kopel <pawel.kopel2@gmail.com>
2022-11-09 00:40:06 +0100

    Change implementation fom wavefront to coloring, add run.sh

--- lab3/membrane.py
+++ lab3/membrane.py
@@ -24,44 +24,63 @@ item_size = MPI.DOUBLE.Get_size()
 
 nbytes = h_size * item_size if rank == 0 else 0
 
-h_win = MPI.Win.Allocate_shared(nbytes, item_size, comm=comm)
-h_buf, alloc_item_size = h_win.Shared_query(0)
-assert alloc_item_size == item_size
-
-h_ary = np.ndarray(buffer=h_buf, dtype='d', shape=h_shape)
-if rank == 0:
-    h_ary.fill(0)
-
 
 def next_h(h, x: int, y: int):
     h[x][y] = (p/T + h[x][y-1] + h[x-1][y] + h[x][y+1] + h[x+1][y])/4
 
+# wavefront
 
-def next_col(h, x: int):
-    for y in range(1, a-1):
-        if x != 1:
-            source = (size+rank-1) % size
-            comm.Recv(np.zeros(1), source=source, tag=y)
+
+# def wavefront(h, x: int):
+#     for y in range(1, a-1):
+#         if x != 1:
+#             source = (size+rank-1) % size
+#             comm.Recv(np.zeros(1), source=source, tag=y)
+#         next_h(h, x, y)
+#         if x + 1 != a:
+#             dest = (rank+1) % size
+#             comm.Isend(np.zeros(1), dest=dest, tag=y)
+
+
+# def next_iter_wavefront(h):
+#     col = rank+1
+#     while col < a-1:
+#         wavefront(h, col)
+#         col += size
+
+
+# coloring
+def coloring(h, x: int, odd: bool):
+    start = 1 + (x % 2) if odd else 2 - (x % 2)
+    for y in range(start, a-1, 2):
         next_h(h, x, y)
-        if x + 1 != a:
-            dest = (rank+1) % size
-            comm.Isend(np.zeros(1), dest=dest, tag=y)
 
 
-def next_iter(h):
+def next_iter_coloring(h):
     col = rank+1
     while col < a-1:
-        next_col(h, col)
+        coloring(h, col, True)
+        col += size
+    comm.Barrier()
+    col = rank+1
+    while col < a-1:
+        coloring(h, col, False)
         col += size
 
 
 comm.Barrier()
 start = MPI.Wtime()
 
+h_win = MPI.Win.Allocate_shared(nbytes, item_size, comm=comm)
+h_buf, alloc_item_size = h_win.Shared_query(0)
+
+h_ary = np.ndarray(buffer=h_buf, dtype='d', shape=h_shape)
+h_ary.fill(0)
+
 for i in range(1000):
-    next_iter(h_ary)
-    comm.Barrier()
-    
+    next_iter_coloring(h_ary)
+
+comm.Barrier()
 end = MPI.Wtime()
 
 if rank == 0:
@@ -69,4 +88,4 @@ if rank == 0:
     upscale = np.ones((scale, scale))
     plt.imsave(f'./result.png', np.kron(h_ary, upscale), cmap='hot')
     np.savetxt("result.csv", h_ary, delimiter=",", fmt='%10.5f')
-    print(f'{a},{p},{T},{end-start}')
+    print(f'{a},{p},{T},{size},{end-start}')
---
+++ lab3/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+## Nazwa zlecenia
+#SBATCH -J MPI-algrow-task4
+## Liczba alokowanych węzłów
+#SBATCH -N 1
+## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
+#SBATCH --ntasks-per-node=12
+## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 4GB na rdzeń)
+#SBATCH --mem-per-cpu=1GB
+## Maksymalny czas trwania zlecenia (format HH:MM:SS)
+#SBATCH --time=01:00:00 
+## Nazwa grantu do rozliczenia zużycia zasobów
+#SBATCH -A plgar2022
+## Specyfikacja partycji
+#SBATCH -p plgrid
+## Plik ze standardowym wyjściem
+#SBATCH --output="output.out"
+## Plik ze standardowym wyjściem błędów
+#SBATCH --error="error.err"
+
+srun /bin/hostname
+
+## Zaladowanie modulu IntelMPI w wersji domyslnej
+module load scipy-bundle/2021.10-intel-2021b
+
+## przejscie do katalogu z ktorego wywolany zostal sbatch
+cd $SLURM_SUBMIT_DIR
+
+echo "a,p,T,threads,time" > times.csv
+
+for i in {1..12}; do
+    for s in 100 200; do
+        for _ in {1..10}; do
+            mpiexec -n $i ./membrane.py $s > times.csv
+        done
+    done
+done
